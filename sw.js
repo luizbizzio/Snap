32fb6e511410eadc70f402d23f966e78e29bf700
Luiz Bizzio <contact@luizbizzio.com>
2025-09-04 14:35:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /*global self, caches*/
 /*jshint esversion: 6*/
 var snapVersion = '11.0.0',
-    cacheName = `snap-pwa-${snapVersion}`,
+    cacheName = `mochicard-pwa-${snapVersion}`,
     filesToCache = [
         'index.html',
 
